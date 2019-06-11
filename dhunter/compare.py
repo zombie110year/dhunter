9b5eda91c89b2cb475de9a9b6e050b0feca61794
zombie110year <zombie110year@outlook.com>
2019-06-11 18:11:53 +0800

    修复重复 scan 出错的 bug

--- dhunter/compare.py
+++ dhunter/compare.py
@@ -21,18 +21,17 @@ def compareFile():
         _file_obj   = Path(path)
         _mtime = _file_obj.stat().st_mtime
 
-        _stored = FILEINFO.select(path)
-        if _stored is None:
+        _stored_mtime = FILEINFO.select(path)
+        if _stored_mtime is None or _stored_mtime < _mtime:
             # 新增此条目
+            print(f"newfile: {path}")
             _hash = md5()
             _hash.update(_file_obj.read_bytes())
             _hash = _hash.hexdigest()
             FILEINFO.insert(path, _hash, _mtime)
-        else: # 判断是否需要更新
-            if _stored.mtime < _mtime:
-                # 现存文件更新
-                print(f"newfile: {path}")
-                _hash = md5()
-                _hash.update(_file_obj.read_bytes())
-                _hash = _hash.hexdigest()
-                FILEINFO.update(path, _hash, _mtime)
+        else:
+            # 现存文件不需要更新, 直接复制前面的记录即可
+            _hash = md5()
+            _hash.update(_file_obj.read_bytes())
+            _hash = _hash.hexdigest()
+            FILEINFO.update(path)
--- dhunter/database.py
+++ dhunter/database.py
@@ -34,23 +34,25 @@ class MemoryDict:
     """
 
     def __init__(self, db_path):
-        self._datum = dict()    # 以 path-FileItem 为键值对的数据库
-        self._index = dict()    # 以 hash-{path, } 为键值对的索引
+        self._old_datum = dict()    # 旧的数据库, 从 .fileinfo.db 中读取而来
+        self._old_index = dict()    # 旧的索引, 从 .fileinfo.db 中读取而来
+        self._datum = dict()    # 以 path-FileItem 为键值对的数据库, 每次 scan 都会新建
+        self._index = dict()    # 以 hash-{path, } 为键值对的索引, 每次 scan 都会新建
         self._db_path = db_path
         self._load()
 
     def select(self, path: str) -> float:
-        """从数据库或索引中以绝对路径为键查询一个值
+        """从旧数据库或索引中以绝对路径为键查询一个值
         如果存在, 则返回对应的 mtime, 否则, 返回 None
         """
-        _item = self._datum.get(path, None)
+        _item = self._old_datum.get(path, None)
         if not _item is None:
             return _item.mtime
         else:
             return None
 
     def insert(self, path: str, hash: str, mtime: float):
-        """新增一个条目
+        """向新数据库新增一个条目
 
         :param str key: hash 值的 字符串表示
         :param str value: 文件的绝对路径
@@ -65,68 +67,57 @@ class MemoryDict:
             self._index[hash] = set()
         self._index[hash].add(path)
 
-    def update(self, path: str, hash: str, mtime: float):
-        """更新一个条目, 有可能在索引中移动此条目
+    def update(self, path: str):
+        """原件相同, 不需要重新计算 hash 值,
+        从旧数据库中向新数据库直接复制数据
 
-        :param str key: hash 值的 字符串表示
-        :param str value: 文件的绝对路径
-        :param float mtime: 文件的最后修改时间
+        :param str path: 绝对路径
         """
         # 查找旧记录
-        _old_item = self._datum[path]
-        # 插入新记录
-        _item = FileItem(path, hash, mtime)
-        self._datum[path] = _item
-        # 更新索引
-        self._index[_old_item.hash].remove(_old_item.path)
-        self._index[hash].add(path)
+        _old_item = self._old_datum[path]
+        self.insert(_old_item.path, _old_item.hash, _old_item.mtime)
 
     def query(self, hash):
-        """获取具有相同 hash 值的 FileItem 集合,
+        """从旧记录中获取具有相同 hash 值的 FileItem 集合,
         按照 mtime 的顺序排序
         """
-        _item_list = list(self._index[hash])
-        _item_list.sort(key=lambda x: self._datum[x].mtime)
+        _item_list = list(self._old_index[hash])
+        _item_list.sort(key=lambda x: self._old_datum[x].mtime)
         return _item_list
 
     def hashs(self):
         """获取数据库中已存储的所有 hash 值
         """
 
-        return self._index.keys()
+        return self._old_index.keys()
 
     def duped_hashs(self):
         """获取数据库中重复条目的 hash 值
         """
 
         result = []
-        for hash in self._index.keys():
-            if len(self._index[hash]) >= 2:
+        for hash in self._old_index.keys():
+            if len(self._old_index[hash]) >= 2:
                 result.append(hash)
 
         return result
 
     def _load(self):
         """从 cwd 中读取 .fileinfo.db 内容
-        将其设为 self._datum 和 self._index
+        将其设为 self._old_datum 和 self._old_index
         """
         _db_path = Path(self._db_path)
         if _db_path.exists():
             _db = _db_path.open("rb")
-            self._datum, self._index = pickle.load(_db)
+            self._old_datum, self._old_index = pickle.load(_db)
             _db.close()
 
     def _dump(self):
+        """保存本次 scan 的结果
+        """
         _db_path = Path(self._db_path)
         if not _db_path.exists():
             _db_path.touch()
-        else:
-            print(
-                "{}: file exists, overwrite? [y/n]".format(self._db_path), file=sys.stderr, end=" ")
-            _ = input()
-            _overwrite = True if _ == "y" else False
-            if not _overwrite:
-                return
 
         _db = _db_path.open("wb")
         pickle.dump((self._datum, self._index), _db)
